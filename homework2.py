5e45133bf955b615ee53bb48e8ead48ddf17ae96
tykhan <tykhanvasyl@gmail.com>
2020-03-25 21:25:33 +0200

    Task 2 completed

--- homework2.py
+++ homework2.py
@@ -1,5 +1,15 @@
 #task2.1
+import numpy  
+
 some_numbers = 7549
-get_list = [7, 5, 4, 9]
+
+get_list = list(map(int,str(some_numbers)))
 print (numpy.prod(get_list))
 
+#task2.2
+get_list.reverse()
+print (get_list)
+
+#task2.3
+get_list.sort()
+print (get_list)
