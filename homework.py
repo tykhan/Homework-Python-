7c27ff4f3526621ddcee84e9d7a4fb922e028e9f
tykhan <tykhanvasyl@gmail.com>
2020-03-25 18:40:00 +0200

    Task 1.1 and 1.2

---
+++ homework.py
@@ -0,0 +1,31 @@
+zen_of_python = """The Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!"""
+
+
+#task1.1
+print ((zen_of_python).upper())
+
+#task1.2
+a = print(zen_of_python.count('never'))
+b = print(zen_of_python.count('better'))
+c = print(zen_of_python.count('is'))
+print (a + b + c)
