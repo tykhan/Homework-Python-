b0d02fee6fdd6a41a239023e058355a02837df3c
tykhan <tykhanvasyl@gmail.com>
2020-03-28 13:42:52 +0200

    Task 1.1 and 1.2

---
+++ homework1_1.py
@@ -0,0 +1,9 @@
+a = int(input("First value: "))
+b = int(input("Second value: "))
+
+print("a + b =",a + b)
+print("a - b =", a - b)
+print("a * b =", a * b)
+print ("a / b =", a / b)
+
+
---
+++ homework1_2.py
@@ -0,0 +1,7 @@
+user_name = (input("What is your name? "))
+user_age = (input("How old are you? "))
+user_city = (input("Where do you live? "))
+
+print("Hello,", user_name)
+print("You are", user_age, "years old.")
+print("You live in", user_city)
