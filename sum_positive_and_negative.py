c14b3f05a41295850441f37be298ac20b3af31fb
tykhan <tykhanvasyl@gmail.com>
2020-04-11 15:54:57 +0300

    homework

---
+++ sum_positive_and_negative.py
@@ -0,0 +1,12 @@
+def count_positives_sum_negatives(arr):
+    if not arr:
+        return []
+    count_num = 0
+    sum_of_num = 0
+    for element in arr:
+        if element > 0:
+            count_num += 1
+        elif element < 0:
+            sum_of_num += element
+    return [count_num, sum_of_num]
+
